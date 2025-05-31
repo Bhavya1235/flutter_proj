e090bd7bc35493457376a5b48ff6cf3c6adf4cb2
Ethnotech <bhavyamr084@gmail.com>
2025-05-31 12:59:17 +0530

    tagged version

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-// ----------------- MAIN PAGE -----------------
+// ----------------- MAIN PAGE -------------------
 class MainPage extends StatelessWidget {
   final TextEditingController _searchController = TextEditingController();
 
